49e6e7623d0d668bc5ee83aaf7f0542ac6a81b13
Cédric <cedric@opla-studio.com>
2015-01-22 00:18:13 +0100

    Menu enhanced
    -fixed menu
    -new animation for desktop
    -close menu after touch item for mobile/tablet
    -adjust margin anchor link
    -sync item menu with section content

--- javascript/common.js
+++ javascript/common.js
@@ -1,6 +1,10 @@
 $(document).ready(function()
 {
+    var cwindow = $(window);
     var action = false;
+    var header = $('#header');
+    var control_menu_mobile = $('#control-menu-mobile');
+    var header_menu = $("#header-menu");
     
 
     /* ==========================================================================
@@ -24,13 +28,24 @@ $(document).ready(function()
         $(menu_active).addClass('on');
     }
 
+    // open/close menu mobile
     var method_menu_mobile = '';
-    $('#control-menu-mobile').on('click', function() {
+    control_menu_mobile.on('click', function() {
         method_menu_mobile = ($(this).hasClass('on')) ? 'slideUp' : 'slideDown';
         $('#header-menu').velocity(method_menu_mobile, { duration: 500 })
         $(this).toggleClass('on');
     });
+
+    // close menu mobile when click on item
+    $('#header-menu li').on('click', function()
+    {
+        if(control_menu_mobile.hasClass('on'))
+        {
+            control_menu_mobile.click();
+        }
+    });
     
+    // open/close menu lang
     $('#control-lang').on('click', function() {
         $(this).toggleClass('on');
     });
@@ -100,20 +115,83 @@ $(document).ready(function()
        Smooth Scrolling Anchor
        ========================================================================== */
 
-    $('a[href*=#]:not([href=#])').click(function() {
+    $('a[href*=#]:not([href=#])').click(function(e)
+    {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-            || location.hostname == this.hostname) {
+            || location.hostname == this.hostname)
+        {
+            var href = $(this).attr("href");
+            var offset_top;
 
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-                if (target.length) {
-                    $('html,body').velocity(
-                        'scroll', 
-                        {offset: target.offset().top, duration: 1000}
-                    );
-                return false;
+            // manual config target
+            switch(href) 
+            { 
+                case "#m-contact": 
+                    offset_top = $(href).offset().top-header_menu_height+1;
+                    break; 
+                default: 
+                    offset_top = $(href).offset().top-header_menu_height_comfort+1;
+                    break; 
             }
+
+            $('html,body').velocity(
+                'scroll', 
+                {offset: offset_top, duration: 500}
+            );
+
+            if(e.preventDefault) e.preventDefault();
+            e.returnValue = false;
         }
+    });
+
+
+    /* ==========================================================================
+       Affix menu
+       ========================================================================== */
+
+    // Cache selectors
+    var lastId,
+        slideshow_height = $('#slideshow').outerHeight(),
+        header_menu_height = 60,
+        header_menu_height_comfort = header_menu_height + 40, // header menu mobile size + margin comfort
+        // All list items
+        menu_items = header_menu.find("a"),
+        // Anchors corresponding to menu items
+        scrollItems = menu_items.map(function()
+        {
+            var item = $($(this).attr("href"));
+            if (item.length) { return item; }
+        });
+
+    // Bind to scroll
+    cwindow.scroll(function()
+    {
+        // Get container scroll position
+        var from_top = cwindow.scrollTop();
+        var from_top_menu = from_top + header_menu_height_comfort;
+
+        // fixed menu
+        if(from_top > slideshow_height) header.addClass('fixed');
+        else header.removeClass('fixed');
+
+        // Get id of current scroll item
+        var cur = scrollItems.map(function()
+        {
+            if ($(this).offset().top < fromTopMenu) return this;
+        });
+
+        // Get the id of the current element
+        cur = cur[cur.length-1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (lastId !== id)
+        {
+            lastId = id;
+            // Set/remove active class
+            menu_items
+            .parent().removeClass("on")
+            .end().filter("[href=#"+id+"]").parent().addClass("on");
+        }                   
     });
 
 });
@@ -125,8 +203,10 @@ window.onload = function()
        Slideshow
        ========================================================================== */
 
-    if($('#slideshow').length)
+    var slideshow = $('#slideshow');
+
+    if(slideshow.length)
     {
-        $('#slideshow').bxSlider({controls:false, auto:true, pause:6500});
+        slideshow.bxSlider({controls:false, auto:true, pause:6500});
     }
 }
